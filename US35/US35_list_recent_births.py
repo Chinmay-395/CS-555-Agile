a379f21fa8dde4ae2e7c5ac1ce6ede015127feb8
Sachin Devangan <dsachin.98@gmail.com>
2023-10-01 21:19:35 -0400

    remove commented code

--- US35/US35_list_recent_births.py
+++ US35/US35_list_recent_births.py
@@ -11,16 +11,16 @@ def listRecentBirths(individuals, days=30):
             # Calculate the number of days between birthdate and current date
             days_since_birth = (current_date - birthdate).days
             
-            # Check if the number of days is within the specified limit (default is 30 days)
+            
             if days_since_birth <= days:
                 # If the condition is met, add the individual's ID, birthdate, and name to the recent_births list
                 recent_births.append((row['ID'], row['BIRTHDAY'], row['NAME']))
                 
-    # Check if there are any recent births found
+   
     if len(recent_births) > 0:
-        # If there are recent births, print the list of recent births
+
         print('US35: List of Recent Births (last', days, 'days):')
         print(recent_births)
     else:
-        # If no recent births are found, print a message indicating that
+
         print('US35: No recent births found in the last', days, 'days.')
