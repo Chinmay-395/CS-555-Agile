9dbc0c8adbe8ab61e7ec96eabe2e87bda1a83335
chinmay395 <chinmaydali395@gmail.com>
2023-10-02 18:57:53 -0400

    fixed all the issues with family data frame for pands

--- Initialparser.py
+++ Initialparser.py
@@ -12,7 +12,7 @@ ID, Married, Divorced, Husband ID, Husband Name, Wife ID, Wife Name, Children
 """This function creates a new list for an individual"""
 def dataFrameOfIndividuals(file_name):
   individuals = []
-  individuals_id_and_name = {}
+  indi_id_name_hmap = {}
   f = open(file_name, "r")
   lines = f.readlines()
 
@@ -36,7 +36,7 @@ def dataFrameOfIndividuals(file_name):
         #   person['index'] = i
         if 'NAME' in line.split():
           person['name'] = line.split('NAME')[1].replace('\n','')
-          individuals_id_and_name[person['id']] = person['name']
+          indi_id_name_hmap[person['id']] = person['name']
         if 'SEX' in line.split() and level == '1':
           person['gender'] = line.split()[2]
         if 'BIRT' in line.split():
@@ -60,7 +60,7 @@ def dataFrameOfIndividuals(file_name):
         #   person['index'] = i
         if 'NAME' in line.split():
           person['name'] = line.split('NAME')[1].replace('\n','')
-          individuals_id_and_name[person['id']] = person['name']
+          indi_id_name_hmap[person['id']] = person['name']
         if 'SEX' in line.split() and level == '1':
           person['gender'] = line.split()[2]
         if 'BIRT' in line.split():
@@ -89,9 +89,94 @@ def dataFrameOfIndividuals(file_name):
 
   f.close()
       
-  return (individuals,individuals_id_and_name)
+  return (individuals,indi_id_name_hmap)
+
+def dataFrameOfFamilies(file_name, indi_id_name_hmap):
+  families = []
+  f = open(file_name, "r")
+  lines = f.readlines()
+
+  indices = []
+  for i in range(0,len(lines)):
+    line = lines[i]
+    level = line[0]
+    if 'FAM' in line.split() and level == '0':
+      indices.append(i)
+  
+  for i in indices:
+    family = {
+    }
+    if indices.index(i) != len(indices)-1:
+      for j in range(i,indices[indices.index(i)+1]):
+        line = lines[j]
+        level = line[0]
+        if 'FAM' in line.split() and level == '0':
+          family['id'] = line.split()[1].replace('@','')
+          family['index'] = i
+        if 'MARR' in line.split():
+          family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
+          family['are divorced'] = False
+        if 'DIV' in line.split():
+          family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
+          family['are divorced'] = True
+        if 'HUSB' in line.split():
+          family['Husband ID'] = line.split()[2].replace('@','')
+          try:
+            family['Husband Name'] = indi_id_name_hmap[family['Husband ID']]
+          except KeyError:
+            family['Husband Name'] = 'nan'
+        if 'WIFE' in line.split():
+          family['Wife ID'] = line.split()[2].replace('@','')
+          try:
+            family['Wife Name'] = indi_id_name_hmap[family['Wife ID']]
+          except KeyError:
+            family['Wife Name'] = 'nan'
+        if 'CHIL' in line.split():
+          if 'children' in family: 
+            family['children'].append(line.split()[2].replace('@',''))
+          else:
+            family['children'] = []
+            family['children'].append(line.split()[2].replace('@',''))
+    else:
+      for j in range(i,len(lines)-1):
+        line = lines[j]
+        level = line[0]
+        if 'FAM' in line.split() and level == '0':
+          family['id'] = line.split()[1].replace('@','')
+          family['index'] = i
+        if 'MARR' in line.split():
+          family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
+          family['are divorced'] = False
+        if 'DIV' in line.split():
+          family['are divorced'] = True
+          family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
+        if 'HUSB' in line.split():
+          family['Husband ID'] = line.split()[2].replace('@','')
+          try:
+            family['Husband Name'] = indi_id_name_hmap[family['Husband ID']]
+          except KeyError:
+            family['Husband Name'] = 'nan'
+        if 'WIFE' in line.split():
+          family['Wife ID'] = line.split()[2].replace('@','')
+          try:
+            family['Wife Name'] = indi_id_name_hmap[family['Wife ID']]
+          except KeyError:
+            family['Wife Name'] = 'nan'
+        if 'CHIL' in line.split():
+          if 'children' in family: 
+            family['children'].append(line.split()[2].replace('@',''))
+          else:
+            family['children'] = []
+            family['children'].append(line.split()[2].replace('@',''))
+
+    families.append(family)
+
+  f.close()
+
+  return families
 
 """This function parses the GEDCOM File and returns 2 lists: one for individuals and another for families"""
 def parse(file_name):
-    (individual_list_of_list, indi_name_id) = dataFrameOfIndividuals(file_name)
-    return (individual_list_of_list, indi_name_id)
+    (individual_list_of_list, indi_id_name_hmap) = dataFrameOfIndividuals(file_name)
+    family_list_of_list = dataFrameOfFamilies(file_name, indi_id_name_hmap)
+    return (individual_list_of_list, family_list_of_list)
