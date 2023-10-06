d0a46884708f90b1aa42c658c3ed4d2b2a3114ce
chinmay395 <chinmaydali395@gmail.com>
2023-10-06 19:03:06 -0400

    added definition of the python function

--- printOutput.py
+++ printOutput.py
@@ -2,6 +2,15 @@ import pandas as pd
 from prettytable import PrettyTable
 
 def printingAllTheStuff(US_name,indi_df, fam_df, errors, anomalies):
+    """The output of the every table will be in a single as well as the console for logging the issues
+
+    Args:
+        US_name (str): name of the user story
+        indi_df (pandas dataframe): valid dataframe of the individuals 
+        fam_df (pandas dataframe): valid dataframe of the families
+        errors (List[str]): list of all the errors in a string format
+        anomalies (List[str]): list of all the anomalies in a string format
+    """
     with open("output.txt", "w") as file:
         ans = indi_df.to_string(index=False)
         ans1 = fam_df.to_string(index=False) 
