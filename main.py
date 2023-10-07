07b209c3a3c464c90eaacde5a854afa45c61e40a
chinmay395 <chinmaydali395@gmail.com>
2023-10-07 17:50:16 -0400

    US04 works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from US31.US31_list_living_single import listLivingSinglesOver30
 from US35.US35_list_recent_births import listRecentBirths
 from US29.US29_list_deceased import listDeceased
 from US_04.UseCase_04 import test_marriage_after_divorce
-
+from US37.US37_list_recent_Survivors import list_recent_survivors
 def main(file_name):
     list_indi, list_fam = parse(file_name)
     # print("THE TOTAL NUMBER OF PEOPLE IN THE LIST", len(list_indi))
@@ -13,8 +13,8 @@ def main(file_name):
     
     df_indi = DataFrame(list_indi)
     df_fam = DataFrame(list_fam)
-    # print("DF \n ", df_indi, "\n")
-    # print("DF \n ", df_fam, "\n")
+    print("DF indi\n ", df_indi, "\n")
+    print("DF fam \n ", df_fam, "\n")
     # run sa3.ged
     listOrphans(df_indi,df_fam)
 
@@ -24,7 +24,14 @@ def main(file_name):
 
     print("List of Deceased People:", listDeceased(df_indi))
 
-    print("LIST OF ERRORS IN US_04 ", test_marriage_after_divorce(df_fam,df_indi))
+    test_marriage_after_divorce(df_fam,df_indi)
+    list_recent_survivors(df_indi, df_fam)
 
 
 main("Sachin_Devangan_CS_555_WS4.ged")
+# print("--------------------------------------------- \n")
+# main("sa2.ged")
+# print("--------------------------------------------- \n")
+# main("sa3.ged")
+# print("--------------------------------------------- \n")
+# main("test_ged_file.ged")
