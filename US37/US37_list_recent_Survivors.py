89b95e46a57b6b460529bb03a1010ce42ea737c8
chinmay395 <chinmaydali395@gmail.com>
2023-10-09 12:26:02 -0400

    US37 is working

---
+++ US37/US37_list_recent_Survivors.py
@@ -0,0 +1,46 @@
+# User Story 37
+
+from datetime import date, datetime
+import pandas as pd
+from printOutput import printingAllTheStuff
+def is_within_last_30_days(event_date):
+    return (date.today() - datetime.strptime(event_date," %d %b %Y").date()).days <= 30 if pd.notna(event_date) else False
+    
+# List all living spouses and descendants of people in a GEDCOM file who died in the last 30 days
+# Assuming spouses mean they were married at one point and don't have to be together
+def list_recent_survivors(individuals, families):
+    print("HELLOOO")
+    recent_survivors = []
+    for index, row in individuals.iterrows():
+        if not row['ALIVE']:
+            if is_within_last_30_days(row['DEATH']):
+                print("THE NAME: ",row['NAME'])
+                living_spouses, living_descendants = [], []
+                for family in row['SPOUSE']:
+                    family_row = families.loc[families['ID'] == family]
+                    print("the family row", family_row)
+                    # living_spouses and living_descendents lists are unsorted
+                    gender_column_string = 'HUSBAND ID' if row['GENDER'] != 'M' else 'WIFE ID'
+                    spouse_id = family_row.at[family_row.index[0], gender_column_string]
+
+                    spouse_row = individuals.loc[individuals['ID'] == spouse_id]
+                    spouse_age = spouse_row.at[spouse_row.index[0], 'AGE']
+                    spouse_alive = spouse_row.at[spouse_row.index[0], 'ALIVE']
+                    living_spouses.append((spouse_id, spouse_age)) if spouse_alive else None
+
+                    children_list = family_row.at[family_row.index[0], 'CHILDREN']
+                    descendants_rows = individuals.loc[individuals['ID'].isin(children_list) & individuals['ALIVE'] == True]
+
+                    for index, descendant in descendants_rows.iterrows():
+                        living_descendants.append((descendant['ID'], descendant['AGE']))
+
+                # print("THE ENTIRE ROW", row)
+                new_recent_survivors = {
+                    'name': (row['ID'], row['AGE']),
+                    'living spouses': living_spouses, 
+                    'living descendants': living_descendants
+                }
+                recent_survivors.append(new_recent_survivors)
+
+    # print("US37: The list of all living spouses and descendants of people who died in the last 30 days: " + str(recent_survivors))
+    # printingAllTheStuff(pd.DataFrame())
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from US35.US35_list_recent_births import listRecentBirths
 from US29.US29_list_deceased import listDeceased
 from US_04.UseCase_04 import test_marriage_after_divorce
 from US37.US37_list_recent_Survivors import list_recent_survivors
+from US27.US27_include_age import include_individual_ages
 def main(file_name):
     list_indi, list_fam = parse(file_name)
     # print("THE TOTAL NUMBER OF PEOPLE IN THE LIST", len(list_indi))
@@ -23,9 +24,11 @@ def main(file_name):
     listRecentBirths(df_indi)
 
     print("List of Deceased People:", listDeceased(df_indi))
-
+    
     test_marriage_after_divorce(df_fam,df_indi)
     list_recent_survivors(df_indi, df_fam)
+
+    include_individual_ages(df_indi, df_fam)
 
 
 main("Sachin_Devangan_CS_555_WS4.ged")
