1c6f99f7762c4cdf5674e4345f00528391cba895
Nouman Syed <syednouman2114@gmail.com>
2023-10-16 15:27:19 -0400

    Done with testing for US39

--- US39/US39_list_upcoming_anniversaries.py
+++ US39/US39_list_upcoming_anniversaries.py
@@ -1,5 +1,6 @@
-import pandas as pd
 from datetime import datetime, timedelta
+
+import pandas as pd
 
 """
 This is the second attempt. Refactored some of the code and reduced complexity. Also made it much more readable
@@ -8,15 +9,16 @@ This is the second attempt. Refactored some of the code and reduced complexity. 
 
 # Check if the spouse with the given ID is alive.
 def is_spouse_alive(individuals, spouse_id):
-    spouse_info = individuals[individuals['ID'] == spouse_id]
-    return spouse_info['ALIVE'].any() if not spouse_info.empty else False
+    spouse_info = individuals[individuals["ID"] == spouse_id]
+    return spouse_info["ALIVE"].any() if not spouse_info.empty else False
 
 
 # Check if the anniversary is within the next 'days' days.
 def is_anniversary_within_range(marriage_date, days=30):
     current_date = datetime.now()
     anniversary_in_range = (
-        current_date + timedelta(days=days)) >= marriage_date >= current_date
+        (current_date + timedelta(days=days)) >= marriage_date >= current_date
+    )
     return anniversary_in_range
 
 
@@ -25,18 +27,23 @@ def listUpcomingAnniversaries(individuals, family, days=30):
     upcoming_anniversaries = set()
 
     for index, row in family.iterrows():
-        husband_id, wife_id = row['HUSBAND ID'], row['WIFE ID']
+        husband_id, wife_id = row["HUSBAND ID"], row["WIFE ID"]
 
         # Check if either the husband or wife is alive
-        if is_spouse_alive(individuals, husband_id) or is_spouse_alive(individuals, wife_id):
+        if is_spouse_alive(individuals, husband_id) or is_spouse_alive(
+            individuals, wife_id
+        ):
             marriage_date = datetime.strptime(
-                row['MARRIED'].strip(), '%d %b %Y')
+                row["MARRIED"].strip(), "%d %b %Y")
             # Adjust the year of marriage date to the current year
             marriage_date = marriage_date.replace(year=datetime.now().year)
 
             if is_anniversary_within_range(marriage_date, days):
                 anniversary_tuple = (
-                    row['HUSBAND NAME'], row['WIFE NAME'], marriage_date.strftime('%d %b'))
+                    row["HUSBAND NAME"],
+                    row["WIFE NAME"],
+                    marriage_date.strftime("%d %b"),
+                )
                 upcoming_anniversaries.add(anniversary_tuple)
 
     if len(upcoming_anniversaries) == 0:
