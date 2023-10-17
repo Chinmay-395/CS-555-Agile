f9658bd08b2bd11f24f1c20d784771d44318adc5
chinmay395 <chinmaydali395@gmail.com>
2023-10-16 21:40:03 -0400

    sprint 2 completed from CD

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from pandas import DataFrame, read_csv
-
-import pandas as pd
+from pandas import DataFrame
 from Initialparser import parse
 from US03.US03_birth_before_death import birthBeforeDeath
 from US29.US29_list_deceased import listDeceased
 from US31.US31_list_living_single import listLivingSinglesOver30
 from US33.US33_list_orphans import listOrphans
 from US35.US35_list_recent_births import listRecentBirths
-from US_04.UseCase_04 import test_marriage_after_divorce
+from US04.UseCase_04 import test_marriage_after_divorce
 from US37.US37_list_recent_Survivors import list_recent_survivors
 from US27.US27_include_age import include_individual_ages
 from US05.UseCase_05 import test_marriage_before_death
-
+from US08.US08_birth_before_marr import birth_before_marriage_of_parents
+from US06.US_06 import test_divorce_before_death
+from US13.US_13 import check_sibling_birth_dates
+# US40 won't be included its a generic function for printing errors
 
 def main(file_name):
     list_indi, list_fam = parse(file_name)
@@ -20,26 +21,38 @@ def main(file_name):
     df_indi = DataFrame(list_indi)
     df_fam = DataFrame(list_fam)
     print("DF indi\n ", df_indi, "\n")
+    df_indi.to_csv("individual_tb.csv", encoding='utf-8', index=False)
     print("DF fam \n ", df_fam, "\n")
+    df_fam.to_csv("family_tb.csv", encoding='utf-8', index=False)
     # run sa3.ged
     listOrphans(df_indi, df_fam)
 
-    # run Sachin_Devangan_CS_555_WS4.ged
+    # # run Sachin_Devangan_CS_555_WS4.ged
     listLivingSinglesOver30(df_indi)
     listRecentBirths(df_indi)
 
     print("US29: List of Deceased People:", listDeceased(df_indi)) #
-    # US04	Marriage before divorce
+    # # US04	Marriage before divorce
     test_marriage_after_divorce(df_fam,df_indi)
-    # US37	List recent survivors
+    # # US37	List recent survivors
     list_recent_survivors(df_indi, df_fam)
 
-    include_individual_ages(df_indi, df_fam) # this is from sprint-2 
+    # include_individual_ages(df_indi, df_fam) # this is from sprint-2 
     print("US03: Birth before death \n", birthBeforeDeath(df_indi))
-    test_marriage_before_death(df_fam,df_indi)
+    
+    birth_before_marriage_of_parents(df_indi,df_fam) # US08
+
+    print("US06: DIVORCE BEFORE DEATH",test_divorce_before_death(df_fam,df_indi))
+
+    print("US13: Siblings spacing", check_sibling_birth_dates(df_indi,df_fam))
+
+    include_individual_ages(df_indi,df_fam)
 
 
-main("Sachin_Devangan_CS_555_WS4.ged")
+
+
+
+main("sa4.ged")
 # print("--------------------------------------------- \n")
 # main("sa2.ged")
 # print("--------------------------------------------- \n")
