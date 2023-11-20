3c1e45fcbd8be7df1645214d26450fc91144aafd
Chinmay-395 <chinmaydali395@gmail.com>
2023-11-20 17:25:37 -0500

    fixed the issues while testing the code

--- US12/US12_parents_not_too_old.py
+++ US12/US12_parents_not_too_old.py
@@ -2,11 +2,11 @@ import math
     
 def parents_not_too_old(individuals,families):
     for index, row in families.iterrows():
-        if not math.isnan(row.children):
-            mother_age=individuals.loc[individuals['ID']==row['WIFE ID'],'age'].iloc[0]
-            father_age=individuals.loc[individuals['ID']==row['HUSBAND ID'],'age'].iloc[0]
-            for child in row.children:
-                child_age=individuals.loc[individuals['id']==child,'age'].iloc[0]
+        if isinstance(row['CHILDREN'],list) and len(row['CHILDREN'])>0:
+            mother_age=individuals.loc[individuals['ID']==row['WIFE ID'],'AGE'].iloc[0]
+            father_age=individuals.loc[individuals['ID']==row['HUSBAND ID'],'AGE'].iloc[0]
+            for child in row['CHILDREN']:
+                child_age=individuals.loc[individuals['ID']==child,'AGE'].iloc[0]
                 if (mother_age-child_age)>=60 or (father_age-child_age)>=80:                     
-                        print("ERROR: FAMILY: US12: " + " FAMILY ID: " + row['ID'] + ": " + row['id'] + " Age of child: " + str(child_age) + " Age of mother: " + str(mother_age)+ " Age of Father: "+ str(father_age))     
+                        print("ERROR: FAMILY: US12: " + " FAMILY ID: " + row['ID'] + ": " + row['ID'] + " Age of child: " + str(child_age) + " Age of mother: " + str(mother_age)+ " Age of Father: "+ str(father_age))     
     
