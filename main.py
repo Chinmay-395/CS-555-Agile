56bee36535cbcdebc34847236665565e60a9c5c0
Chinmay-395 <chinmaydali395@gmail.com>
2023-11-20 15:30:25 -0500

    chronologically arranged all imported functions

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from US28.US28_order_siblings_by_age import order_siblings_by_age
 from US29.US29_list_deceased import listDeceased
 from US30.US30_list_living_married import list_living_married
 from US31.US31_list_living_single import listLivingSinglesOver30
-from US32.US32_List_multiple_births import *
+from US32.US32_List_multiple_births import listMultipleBirths
 from US33.US33_list_orphans import listOrphans
 from US35.US35_list_recent_births import listRecentBirths
 from US36.US36_list_recent_deaths import listRecentDeaths
@@ -41,55 +41,66 @@ def main(file_name):
     print("The Family Table \n ", df_fam, "\n")
     df_fam.to_csv("family_tb.csv", encoding='utf-8', index=False)
 
-    # run sa3.ged
-    listOrphans(df_indi, df_fam) # US33
+    
+    dates_before_current_date(df_indi,df_fam) # US01
+    validateBirthBeforeMarriage(df_indi,df_fam) #US02
 
-    # # run Sachin_Devangan_CS_555_WS4.ged
-    listLivingSinglesOver30(df_indi)
-    listRecentBirths(df_indi)
-    print("US38: List of Upcoming Birthdays", listUpcomingBirthdays(df_indi))
-    # Examples:
-    # print(parse_partial_date("1990"))        # Output: 1990-01-01
-    # print(parse_partial_date("MAR 1990"))    # Output: 1990-03-01
-    # print(parse_partial_date("15 MAR 1990"))  # Output: 1990-03-15
-    # print(parse_partial_date("JAN 15"))      # Output: None (Invalid, unable to parse)
-    listValidMarriages(df_fam, df_indi)
-    validateBirthBeforeMarriage(df_indi, df_fam)
-    check_unique_ids(df_indi, df_fam)
-    result = order_siblings_by_age(df_indi, df_fam)
-    print(result)
 
-    print("US29: List of Deceased People:", listDeceased(df_indi))
-    # US04	Marriage before divorce
-    # test_marriage_after_divorce(df_fam, df_indi)
-    # US37	List recent survivors
-
-    list_recent_survivors(df_indi, df_fam)
-
-    # include_individual_ages(df_indi, df_fam) # this is from sprint-2 
-    print("US03: Birth before death")
+    print("US03: Birth before death")#US03
     print("", birthBeforeDeath(df_indi))
+    print("US04: Marriage after divorce")#US04
+    test_marriage_after_divorce(df_fam,df_indi)
+    print("US05: MARRIED BEFORE DEATH")#US05
+    print("",test_marriage_before_death(df_indi,df_fam))
+
+    print("US06: DIVORCE BEFORE DEATH")#US06
+    print("",test_divorce_before_death(df_fam,df_indi))
+
+    less_than_150_years(df_indi) # US07
     
     birth_before_marriage_of_parents(df_indi,df_fam) # US08
 
-    print("US06: DIVORCE BEFORE DEATH")
-    print("",test_divorce_before_death(df_fam,df_indi))
+    listValidMarriages(df_fam, df_indi) #US10
 
-    print("US13: Siblings spacing")
+    print("US12: parents not too old of the children")#US12
+    parents_not_too_old(df_indi,df_fam)
+
+    print("US13: Siblings spacing")#US13
     print("",check_sibling_birth_dates(df_indi,df_fam))
-    print("US27: Include age of individuals:")
+    
+
+    check_unique_ids(df_indi, df_fam)#US22
+
+    print("US27: Include age of individuals:")#US27
     include_individual_ages(df_indi,df_fam)
-    print("US05: MARRIED BEFORE DEATH")
-    print("",test_marriage_before_death(df_indi,df_fam))
-    print("US39: List of Upcoming anverseries")
-    print("",listUpcomingAnniversaries(df_indi,df_fam))
-    # 
 
-    listRecentDeaths(df_indi)
+    result = order_siblings_by_age(df_indi, df_fam)#US28
+    print(result)
 
+    
+    print("US29: List of Deceased People:", listDeceased(df_indi))#US29
+    print("US30: List of living married people in the family")#US30
+    list_living_married(df_indi,df_fam)
+    listLivingSinglesOver30(df_indi)#US31
+    print("US32: List of multiple births")
+    listMultipleBirths(df_indi)#US32
+    listOrphans(df_indi, df_fam) # US33
+    listRecentBirths(df_indi)#US35
+    listRecentDeaths(df_indi)#US36
+    list_recent_survivors(df_indi, df_fam)#US37
+    print("US38: List of Upcoming Birthdays", listUpcomingBirthdays(df_indi))#US38
+    print("US39: List of Upcoming anverseries")#US39
+    print("",listUpcomingAnniversaries(df_indi,df_fam))#US40
+    print("US40: Line number where error is in GED file",get_line(2))
+    
+    print("US41: Parsing Partial Dates")# US41
+    print(parse_partial_date("1990"))        # Output: 1990-01-01
+    print(parse_partial_date("MAR 1990"))    # Output: 1990-03-01
+    print(parse_partial_date("15 MAR 1990"))  # Output: 1990-03-15
+    print(parse_partial_date("JAN 15"))      # Output: None (Invalid, unable to parse)
+
+    print("US42: Reject Illegitimate Dates")#US42
     print(rejectIllegitimateDates(df_indi, df_fam))
-    dates_before_current_date(df_indi,df_fam) # US01
-    less_than_150_years(df_indi) # US07
 
 
 
